2e93f52a9d6bc92368dda2738e3cfcc59a6f0990
Letizia L. Baptistella <145225061+le-bap@users.noreply.github.com>
2023-09-27 18:42:41 +0000

    Criação da função criar cliente

--- main.c
+++ main.c
@@ -4,7 +4,16 @@
 int main(){
     ListaClientes lt;
 
-    
+    int opcao;
+
+    do{
+        menu();
+        scanf("%d", &opcao);
+
+        if(opcao == 1){
+            NovoCliente(&lt);
+        }
+    }while (opcao != 0);
 
     return 0;
 }
